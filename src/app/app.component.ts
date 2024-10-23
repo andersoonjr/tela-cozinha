eeb412d1b63310ea4c7e38d95737da2f58f221e5
Anderson Junior - 195023 <ti011.anderson@gmail.com>
2024-10-23 19:27:33 -0300

    Tela cozinha completa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TelaCozinhaComponent } from './components/tela-cozinha/tela-cozinha.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet, TelaCozinhaComponent],
+  template: `
+    <app-tela-cozinha></app-tela-cozinha>
+  `,
+  styles: []
 })
 export class AppComponent {
   title = 'tela-cozinha';
